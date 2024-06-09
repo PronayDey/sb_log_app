f006b153881d58908f426391f8d18b1d1bc91bd6
pronay <pronaydey777@gmail.com>
2024-06-09 13:01:06 +0530

    'commited'

---
+++ Kafka_producer_app/src/main/java/in/ashokit/service/OrderService.java
@@ -0,0 +1,24 @@
+package in.ashokit.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import in.ashokit.constants.AppConstants;
+import in.ashokit.order.Order;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private KafkaTemplate<String, Order> kafkaTemplate;
+
+	public String addMsg(Order order) {
+
+		// publish msg to kafka topic
+		kafkaTemplate.send(AppConstants.TOPIC, order);
+
+		return "Msg Published To Kafka Topic";
+	}
+}
+
