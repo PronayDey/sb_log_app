084d98118ee199b048e9a51219f14f33106d61e3
pronay <pronaydey777@gmail.com>
2024-06-26 12:24:08 +0530

    Redis commited

---
+++ Redis_cloud_app/src/main/java/in/ashokit/RedisCloudAppApplication.java
@@ -0,0 +1,13 @@
+package in.ashokit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisCloudAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisCloudAppApplication.class, args);
+	}
+
+}
